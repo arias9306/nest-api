3ad7f5695c48fb3bece6a7dbbc09e17fb7e1745f
Andrés Arias <arias9306@gmail.com>
2020-07-20 16:31:03 -0500

    configuration Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { Configuration } from './config/config.keys';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+
+  constructor(private readonly configService: ConfigService) {
+    AppModule.port = this.configService.get(Configuration.PORT);
+  }
+}
